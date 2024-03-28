78af5671e641315fd6fc23e4ff33e25a80fac1b8
Ella Hoffman <e-hoffman.2@onu.edu>
2024-03-27 21:47:59 -0400

    completed romanCharValue function

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,46 @@
- 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int romanCharValue(char r) {
+    switch (r) {
+    case 'I': return 1;
+    case 'V': return 5;
+    case 'X': return 10;
+    case 'L': return 50;
+    case 'C': return 100;
+    case 'D': return 500;
+    case 'M': return 1000;
+    default: return 0; 
+    }
+}
+
+int main() {
+    string romanNumeral;
+    cout << "Enter a Roman numeral: ";
+    cin >> romanNumeral;
+
+    int decimalValue = 0;
+    int prevValue = 0; 
+
+    
+    for (int i = romanNumeral.length() - 1; i >= 0; i--) {
+        int currentVal = romanCharValue(romanNumeral[i]);
+
+        
+        if (currentVal < prevValue) {
+            decimalValue -= currentVal;
+        }
+        else {
+            decimalValue += currentVal;
+        }
+
+      
+        prevValue = currentVal;
+    }
+
+    cout << "Decimal value: " << decimalValue << endl;
+
+    return 0;
+}
