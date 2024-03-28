3084993cf4f0f63ba702363304533b4db11208a8
Ella Hoffman <e-hoffman.2@onu.edu>
2024-03-27 22:39:02 -0400

    completed convertRomanToInt function

--- Source.cpp
+++ Source.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+
 int romanCharValue(char r) {
     switch (r) {
     case 'I': return 1;
@@ -16,31 +17,48 @@ int romanCharValue(char r) {
     }
 }
 
-int main() {
-    string romanNumeral;
-    cout << "Enter a Roman numeral: ";
-    cin >> romanNumeral;
 
-    int decimalValue = 0;
-    int prevValue = 0; 
+int convertRomanToInt(string s) {
+    int total = 0;
+    int n = s.length();
 
-    
-    for (int i = romanNumeral.length() - 1; i >= 0; i--) {
-        int currentVal = romanCharValue(romanNumeral[i]);
+    while (n > 0) {
+        int firstVal = romanCharValue(s[0]);
+        int secondVal = (n > 1) ? romanCharValue(s[1]) : 0;
 
-        
-        if (currentVal < prevValue) {
-            decimalValue -= currentVal;
+        if (firstVal >= secondVal || n == 1) {
+            total += firstVal;
+            s.erase(0, 1);
         }
         else {
-            decimalValue += currentVal;
+            total += (secondVal - firstVal);
+            s.erase(0, 2); 
         }
 
-      
-        prevValue = currentVal;
+        n = s.length();
     }
 
-    cout << "Decimal value: " << decimalValue << endl;
+    return total;
+}
+
+int main() {
+
+    cout << "Testing romanCharValue function:\n";
+    cout << "Value of M: " << romanCharValue('M') << endl;
+    cout << "Value of C: " << romanCharValue('C') << endl;
+    cout << "Value of X: " << romanCharValue('X') << endl;
+    cout << "Value of V: " << romanCharValue('V') << endl;
+    cout << "Value of I: " << romanCharValue('I') << endl;
+
+  
+    string testRoman1 = "MCMLXXVIII";
+    string testRoman2 = "MMXIX";
+    string testRoman3 = "MCMXCIV";
+
+    cout << "\nTesting convertRomanToInt function:\n";
+    cout << "Roman numeral: " << testRoman1 << ", Decimal value: " << convertRomanToInt(testRoman1) << endl;
+    cout << "Roman numeral: " << testRoman2 << ", Decimal value: " << convertRomanToInt(testRoman2) << endl;
+    cout << "Roman numeral: " << testRoman3 << ", Decimal value: " << convertRomanToInt(testRoman3) << endl;
 
     return 0;
 }
